a8de6a5b22f1528553affb2a23c6928dd2e6821e
goli <fateme.jafariyan06@gmail.com>
2025-06-02 09:29:47 +0330

    final changes

---
+++ js/panel.js
@@ -0,0 +1,5 @@
+const panelButtomEl=document.querySelector('.panelbutton');
+const panelToolEl=document.querySelector('.paneltool');
+panelButtomEl.addEventListener("click", () =>{
+    panelToolEl.style.right='0px';
+})
