b7823a1010257f5da6bb3e83bfdc6fe1ab9ffcb2
goli <fateme.jafariyan06@gmail.com>
2025-06-02 10:01:42 +0330

    finish

--- js/panel.js
+++ js/panel.js
@@ -1,5 +1,11 @@
-const panelButtomEl=document.querySelector('.panelbutton');
-const panelToolEl=document.querySelector('.paneltool');
-panelButtomEl.addEventListener("click", () =>{
-    panelToolEl.style.right='0px';
-})
+const panelButtonEl = document.querySelector(".panelbutton");
+const panelToolEl = document.querySelector(".paneltool");
+panelButtonEl.addEventListener("click", () => {
+  if (panelToolEl.style.right = "-325px") {
+    panelToolEl.style.right="0px";
+  }
+  else if (panelToolEl.style.right = "0px") {
+    panelToolEl.style.removeProperty("right");
+    panelToolEl.style.right="-325px";
+  }
+});
